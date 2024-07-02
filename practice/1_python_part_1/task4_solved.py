46a3243c985cd66e5cc158a2c7b8a7236a2b528e
ekryukova <ekryukova@griddynamics.com>
2024-07-02 18:31:08 +0300

    task4 solved

--- practice/1_python_part_1/task4_solved.py
+++ practice/1_python_part_1/task4_solved.py
@@ -10,4 +10,8 @@ from typing import List
 
 
 def calculate_power_with_difference(ints: List[int]) -> List[int]:
-    ...
+    res = [ints[0]]
+    powers = [i**2 for i in ints]
+    for i in range(1, len(ints)):
+        res.append(powers[i] - (powers[i-1] - ints[i-1]))
+    return res
