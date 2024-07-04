65dae6651520bb3d6b1e6a190bca2ea726ce3b8a
ekryukova <ekryukova@griddynamics.com>
2024-07-04 14:54:56 +0300

    task_input_output corrected

--- practice/2_python_part_2/task_read_write_2_solved.py
+++ practice/2_python_part_2/task_read_write_2_solved.py
@@ -10,6 +10,8 @@ Example:
         file1.txt (content: "abc\ndef\nxyz", encoding: UTF-8)
         file2.txt (content: "xyz,def,abc", encoding: CP1252)
 """
+import sys
+import os
 
 
 def generate_words(n=20):
@@ -22,3 +24,16 @@ def generate_words(n=20):
         words.append(word)
 
     return words
+
+
+path = sys.path[1]+'/practice/2_python_part_2/task_rw2_files'
+if not os.path.exists(path):
+     os.makedirs(path)
+
+with open(path+'/file1.txt', 'w', encoding='utf-8') as f1:
+    f1.writelines('\n'.join(generate_words()))
+
+with open(path+'/file2.txt', 'w', encoding='CP1252') as f2:
+    with open(path+'/file1.txt', 'r', encoding='utf-8') as f1:
+        words_list = list(map(lambda x: x.strip('\n'), f1.readlines()))
+    f2.writelines(','.join(words_list[::-1]))
