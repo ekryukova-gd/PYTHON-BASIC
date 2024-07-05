6f8d0f588bd8a54af048b215526a4ab437057984
ekryukova <ekryukova@griddynamics.com>
2024-07-05 11:01:13 +0300

    corrected, type hints added

--- practice/2_python_part_2/task_classes_solved.py
+++ practice/2_python_part_2/task_classes_solved.py
@@ -28,37 +28,38 @@ PEP8 comply strictly.
 import datetime
 
 
-class Teacher:
-    def __init__(self, last_name, first_name):
-        self.last_name = last_name
-        self.first_name = first_name
-
-    @staticmethod
-    def create_homework(task_text, days_to_complete):
-        return Homework(task_text, days_to_complete)
-
-
-class Student:
-    def __init__(self, last_name, first_name):
-        self.last_name = last_name
-        self.first_name = first_name
-
-    def do_homework(self, homework):
-        if Homework.is_active(homework):
-            return homework
-        else:
-            print('You are late')
-            return None
-
-
 class Homework:
-    def __init__(self, text, days_to_complete):
+    def __init__(self, text: str, days_to_complete: int):
         self.text = text
         self.created = datetime.datetime.now()
         self.deadline = datetime.timedelta(days=days_to_complete)
 
     def is_active(self):
         return datetime.datetime.now() - self.created < self.deadline
+
+
+class Teacher:
+    def __init__(self, last_name: str, first_name: str):
+        self.last_name = last_name
+        self.first_name = first_name
+
+    @staticmethod
+    def create_homework(task_text: str, days_to_complete: int):
+        return Homework(task_text, days_to_complete)
+
+
+class Student:
+    def __init__(self, last_name: str, first_name: str):
+        self.last_name = last_name
+        self.first_name = first_name
+
+    def do_homework(self, homework: Homework):
+        if homework.is_active():
+            return homework
+        else:
+            print('You are late')
+            return None
+
 
 if __name__ == '__main__':
     teacher = Teacher('Dmitry', 'Orlyakov')
