eaf17bf9198c6cd559705df56c86fd5491c9d9f5
ekryukova <ekryukova@griddynamics.com>
2024-07-09 10:36:52 +0300

    test_task_classes - trying to parametrize

--- practice/3_python_testing/conftest.py
+++ practice/3_python_testing/conftest.py
@@ -1,19 +1,66 @@
 import pytest
+from collections import namedtuple
 
 from practice.python_part_2.task_classes_solved import *
 
 
+# ----------------- fixtures for test_task_classes -------------------
+# @pytest.fixture
+# def homework():
+#     return Homework('homework_text_1', 5)
+#
+# @pytest.fixture
+# def teacher():
+#     return Teacher('Ivanov', 'Petr')
+#
+# @pytest.fixture
+# def student():
+#     return Student('Sidorov', 'Ivan')
 
-@pytest.fixture
-def homework():
-    return Homework('homework_text_1', 5)
 
-@pytest.fixture
-def teacher():
-    return Teacher('Ivanov', 'Petr')
+homeworks = [
+    ['homework1', 5],
+    ['homework2', 0],
+#    ['homework3', -1]
+]
 
-@pytest.fixture
-def student():
-    return Student('Sidorov', 'Ivan')
+homework_output = [5, 0]
 
 
+@pytest.fixture(scope='session', params=(homeworks, homework_output))
+def homework(request):
+    return Homework(*request.param), *homework_output
+
+
+teachers = [
+    ('Ivanov', 'Ivan'),
+    ('', ''),
+#    (1231, 4312)
+]
+
+teacher_output = namedtuple('teachers_output', ['last_name', 'first_name'])
+output_teachers = [
+    teacher_output('Ivanov', 'Ivan'),
+    teacher_output('', '')]
+
+
+@pytest.fixture(scope='session', params=(teachers, teacher_output))
+def teacher(request):
+    return Teacher(*request.param), *teacher_output
+
+
+students = [
+    ('Markova', 'Irina'),
+    ('', ''),
+    (789, 987)
+]
+
+student_output = namedtuple('student_output', ['last_name', 'first_name'])
+output_students = [
+    student_output('Markova', 'Irina'),
+    student_output('', '')]
+
+
+@pytest.fixture(scope='session', params=(students, student_output))
+def student(request):
+    return Student(*request.param), *student_output
--- practice/3_python_testing/test_task_classes.py
+++ practice/3_python_testing/test_task_classes.py
@@ -8,43 +8,25 @@ import pytest
 import datetime
 from practice.python_part_2.task_classes_solved import *
 
-# homeworks = [
-#     ['homework1', 5],
-#     ['homework2', 0],
-#     ['homework3', -1]
-# ]
-#
-# teachers = [
-#     ('Ivanov', 'Ivan'),
-#     ('', ''),
-#     (1231, 4312)
-# ]
-@pytest.mark.classes
-# @pytest.mark.parametrize(
-#     'homework',
-#     homeworks,
-#     indirect=True
-# )
-def test_Homework(homework):
-    assert homework.deadline.days == 5
-    assert homework.created.date() == datetime.date.today()
-    assert homework.is_active() is True
 
 @pytest.mark.classes
-# @pytest.mark.parametrize(
-#     'teacher',
-#     teachers,
-#     indirect=True
-# )
+def test_Homework(homework):
+    assert homework[0].deadline.days == homework[1]
+    assert homework[0].created.date() == datetime.date.today()
+ #   assert homework.is_active() is True
+
+
+@pytest.mark.classes
 def test_Teacher(teacher):
-    assert teacher.last_name == 'Ivanov'
-    assert teacher.first_name == 'Petr'
+    assert teacher[0].last_name == teacher[1].last_name
+    assert teacher[0].first_name == teacher[0].first_name
     assert isinstance(teacher.create_homework('homework_text_2', 2), Homework)
+
 
 @pytest.mark.classes
 def test_Student(student, homework):
-    assert student.last_name == 'Sidorov'
-    assert student.first_name == 'Ivan'
+    assert student[0].last_name == student[1].last_name
+    assert student[0].first_name == student[1].first_name
     if homework.is_active():
         assert student.do_homework(homework) == homework
     else:
